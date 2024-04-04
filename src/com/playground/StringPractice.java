4b5dbaf2ca19dfde75ceff320885fad22e07b6e9
dappdavid <dappdavid99@gmail.com>
2024-04-04 14:12:41 +0530

    update

--- src/com/playground/StringPractice.java
+++ src/com/playground/StringPractice.java
@@ -1,8 +1,12 @@
 package com.playground;
 
+import java.util.regex.Pattern;
+
 public class StringPractice {
 
     public static void main(String[] args) {
+        String str = "David John";
+        Pattern.compile("").splitAsStream(str.toLowerCase()).forEach(System.out::println);
 
     }
 }
--- src/com/programs/java8/ProgramsJava8.java
+++ src/com/programs/java8/ProgramsJava8.java
@@ -1,7 +1,41 @@
 package com.programs.java8;
 
+import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class ProgramsJava8 {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        //Sum all the digits of a string
+        String numberString = "456";
+        int sum = numberString.chars() // Create an IntStream
+                .map(Character::getNumericValue) // Convert each char to a numeric value
+                .sum(); // Sum all the numeric values
+
+        //String array to String
+        String[] stringArray = {"Java", "Python", "C++"};
+        String result = Arrays.stream(stringArray)
+                .collect(Collectors.joining(", "));
+
+        //Int array to String
+        int[] intArray = {1, 2, 3, 4, 5};
+        String res = Arrays.stream(intArray)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        //find first non-repeated character in a string
+        String input = "swiss";
+        Character firstNonRepeated = input.chars() // Creates an IntStream
+                .mapToObj(c -> (char) c) // Converts IntStream to Stream<Character>
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) // Collects characters and their counts into a LinkedHashMap
+                .entrySet()
+                .stream() // Streams the entry set of the map
+                .filter(entry -> entry.getValue() == 1) // Filters for characters with a count of 1
+                .map(Map.Entry::getKey) // Maps to the character
+                .findFirst() // Finds the first character that matches the criteria
+                .orElse(null); // Returns null if no character matches
+
     }
 }
